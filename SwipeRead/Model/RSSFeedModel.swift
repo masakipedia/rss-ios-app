13b525a543d14f68369561fe41cfeb58d7e66377
MasakiOkuno <150441030@ccalumni.meijo-u.ac.jp>
2018-02-26 12:01:29 +0900

    [update] RSSFeedModel comment

--- SwipeRead/Model/RSSFeedModel.swift
+++ SwipeRead/Model/RSSFeedModel.swift
@@ -12,19 +12,25 @@ class RSSFeedModel {
     
     // singleton
     static var shared = RSSFeedModel()
-    var allFeedsURLString: [String] = ["https://www.gizmodo.jp/index.xml", "https://www.lifehacker.jp/feed/index.xml"]
+    
     var feed: RSSFeed?
-    var url: URL = URL(string: "https://www.gizmodo.jp/index.xml")!
+    var allFeedsURLString: [String] = ["https://www.gizmodo.jp/index.xml", "https://www.lifehacker.jp/feed/index.xml"]
+    var url: URL!
 
+    
+    /// Return number of items that the current URL has
+    ///
+    /// - Returns: items count
     func itemsCount() -> Int {
         return feed?.items?.count ?? 0
     }
     
+    
     /// Load RSS feed and display it in tableview
     ///
-    /// - Parameter tableView: display tableview
+    /// - Parameter tableView: display at tableview
     func loadRssFeed(tableView: UITableView) {
-        let parser = FeedParser(URL: url)!
+        let parser = FeedParser(URL: self.url)!
     
         // Parse asynchronously, not to block the UI.
         parser.parseAsync { [weak self] (result) in
@@ -37,10 +43,15 @@ class RSSFeedModel {
         }
     }
 
+    
+    /// Return favicon image url. The url is using google api.
+    /// https://www.google.com/s2/favicons?domain="want site url"
+    ///
+    /// - Returns: favicon image url
     func faviconURL() -> URL {
-        var splitURL = url.absoluteString.split(separator: "/")
+        var splitURL = self.url.absoluteString.split(separator: "/")
         guard splitURL.count > 1 else {
-            return url
+            return self.url
         }
         return URL(string: "https://www.google.com/s2/favicons?domain=" + splitURL[0] + "//" + splitURL[1])!
     }
